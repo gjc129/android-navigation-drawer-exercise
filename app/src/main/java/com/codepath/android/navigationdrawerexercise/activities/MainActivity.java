d9c8302a0c6496574f03c8c1b742d1d256e1405d
gjc129 <cabrera.gabriel94@me.com>
2019-02-27 12:20:33 -0800

    Updates Added toobar.xml Added drawer_view.xml added code to main_activity working on setUpDrawerToggle/hamburger icon thing

--- app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
+++ app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
@@ -1,7 +1,10 @@
 package com.codepath.android.navigationdrawerexercise.activities;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.codepath.android.navigationdrawerexercise.R;
 
@@ -12,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tbToolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+
+
+        //find our drawer view
+        DrawerLayout mDrawer = findViewById(R.id.drawerLayout);
+        drawerToggle = setupDrawerToggle(toolbar);
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle(Toolbar toolbar) {
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open, R.string.drawer_close);
     }
 }
